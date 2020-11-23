1a55f79772a521e9cd102e35726e48f8185267b9
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-11-23 20:27:39 +0100

    making moves

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from typing import Dict,Optional
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get('/')
-def root(q1: float , q2 : float , q3: float ) -> Dict:
+@app.get('/api')
+def index():
+    return 'Welcome to this api, go to /api/docs to see documentation'
+
+@app.get('/api')
+def predict(q1: float , q2 : float , q3: float ) -> Dict:
     return {'you':q1}
