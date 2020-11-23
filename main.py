3177527905f3cd5279c097b2b1a94893fc94b4ab
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-11-23 20:38:31 +0100

    e be things

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def index():
     return '<h2>Welcome to this api, go to /api/docs to see documentation</h2>'
 
 @app.get('/api')
-def predict(q1: float , q2 : float , q3: float ) -> Dict:
-    return {'you':q1}
+def predict(q1: Optional[float]=None , q2 : Optional[float]=None , q3: Optional[float]=None ) -> Dict:
+    return {"Q1":q1, "Q2":q2, "Q3":q3}
