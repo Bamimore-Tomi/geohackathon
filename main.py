d8fbe588b4b606858a08eacae29f2a408554a7c8
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-11-23 22:43:40 +0100

    fixed model

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from typing import Dict,Optional
-
+import pickle
+import numpy as np
+from pydantic import BaseModel
 from fastapi import FastAPI
+
 app = FastAPI()
+model = pickle.load(open('model.pkl','rb'))
+
+class Prediction(BaseModel):
+    prediction: int
 
 @app.get('/')
 def index():
-    return '<h2>Welcome to this api, go to /api/docs to see documentation</h2>'
+    return '<h2>Welcome to this api, go to /docs to see documentation</h2>'
 
-@app.get('/api')
-def predict(q1: Optional[float]=None , q2 : Optional[float]=None , q3: Optional[float]=None ) -> Dict:
-    return {"Q1":q1, "Q2":q2, "Q3":q3}
+@app.get('/api', response_model=Prediction)
+def predict(q1: Optional[float]=None , q2 : Optional[float]=None , q3: Optional[float]=None):
+    df_test = np.array([q1,q2,q3])
+    df_test = df_test.reshape(1,-1)
+    prediction = model.predict(df_test)
+    response = {"prediction":prediction[0]}
+    return response
